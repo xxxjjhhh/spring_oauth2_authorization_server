2e90059896079c5c1fa62c438cab52ef0fe84a88
kimjihun <xxxjjhhh@naver.com>
2024-09-18 19:36:06 +0900

    * init

---
+++ src/main/java/com/example/spring_oauth2_authorization_server/SpringOauth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_oauth2_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2AuthorizationServerApplication.class, args);
+    }
+
+}
