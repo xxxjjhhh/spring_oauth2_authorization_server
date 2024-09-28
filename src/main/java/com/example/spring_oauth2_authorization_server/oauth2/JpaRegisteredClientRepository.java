d3169774de564d5b2bdded2eb5bc17990109e58c
kimjihun <xxxjjhhh@naver.com>
2024-09-29 03:18:49 +0900

    * OAuth2 Register 사용 로직 작성 close #4 #6

---
+++ src/main/java/com/example/spring_oauth2_authorization_server/oauth2/JpaRegisteredClientRepository.java
@@ -0,0 +1,151 @@
+package com.example.spring_oauth2_authorization_server.oauth2;
+
+import com.example.spring_oauth2_authorization_server.entity.RegisterEntity;
+import com.example.spring_oauth2_authorization_server.repository.RegisterRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationServerJackson2Module;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class JpaRegisteredClientRepository implements RegisteredClientRepository {
+
+    private final RegisterRepository registerRepository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public JpaRegisteredClientRepository(RegisterRepository registerRepository) {
+        this.registerRepository = registerRepository;
+
+        ClassLoader classLoader = JpaRegisteredClientRepository.class.getClassLoader();
+        List<Module> securityModules = SecurityJackson2Modules.getModules(classLoader);
+        this.objectMapper.registerModules(securityModules);
+        this.objectMapper.registerModule(new OAuth2AuthorizationServerJackson2Module());
+    }
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+        registerRepository.save(toEntity(registeredClient));
+    }
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return registerRepository.findById(id).map(this::toObject).orElse(null);
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        return registerRepository.findByClientId(clientId).map(this::toObject).orElse(null);
+    }
+
+    private RegisteredClient toObject(RegisterEntity entity) {
+        Set<String> clientAuthenticationMethods = StringUtils.commaDelimitedListToSet(entity.getClientAuthenticationMethods());
+        Set<String> authorizationGrantTypes = StringUtils.commaDelimitedListToSet(entity.getAuthorizationGrantTypes());
+        Set<String> redirectUris = StringUtils.commaDelimitedListToSet(entity.getRedirectUris());
+        Set<String> postLogoutRedirectUris = StringUtils.commaDelimitedListToSet(entity.getPostLogoutRedirectUris());
+        Set<String> clientScopes = StringUtils.commaDelimitedListToSet(entity.getScopes());
+
+        RegisteredClient.Builder builder = RegisteredClient.withId(entity.getId())
+                .clientId(entity.getClientId())
+                .clientIdIssuedAt(entity.getClientIdIssuedAt())
+                .clientSecret(entity.getClientSecret())
+                .clientSecretExpiresAt(entity.getClientSecretExpiresAt())
+                .clientName(entity.getClientName())
+                .clientAuthenticationMethods(authenticationMethods ->
+                        clientAuthenticationMethods.forEach(authenticationMethod ->
+                                authenticationMethods.add(resolveClientAuthenticationMethod(authenticationMethod))))
+                .authorizationGrantTypes((grantTypes) ->
+                        authorizationGrantTypes.forEach(grantType ->
+                                grantTypes.add(resolveAuthorizationGrantType(grantType))))
+                .redirectUris((uris) -> uris.addAll(redirectUris))
+                .postLogoutRedirectUris((uris) -> uris.addAll(postLogoutRedirectUris))
+                .scopes((scopes) -> scopes.addAll(clientScopes));
+
+        Map<String, Object> clientSettingsMap = parseMap(entity.getClientSettings());
+        builder.clientSettings(ClientSettings.withSettings(clientSettingsMap).build());
+
+        Map<String, Object> tokenSettingsMap = parseMap(entity.getTokenSettings());
+        builder.tokenSettings(TokenSettings.withSettings(tokenSettingsMap).build());
+
+        return builder.build();
+    }
+
+    private RegisterEntity toEntity(RegisteredClient registeredClient) {
+        List<String> clientAuthenticationMethods = new ArrayList<>(registeredClient.getClientAuthenticationMethods().size());
+        registeredClient.getClientAuthenticationMethods().forEach(clientAuthenticationMethod ->
+                clientAuthenticationMethods.add(clientAuthenticationMethod.getValue()));
+
+        List<String> authorizationGrantTypes = new ArrayList<>(registeredClient.getAuthorizationGrantTypes().size());
+        registeredClient.getAuthorizationGrantTypes().forEach(authorizationGrantType ->
+                authorizationGrantTypes.add(authorizationGrantType.getValue()));
+
+        RegisterEntity entity = new RegisterEntity();
+        entity.setId(registeredClient.getId());
+        entity.setClientId(registeredClient.getClientId());
+        entity.setClientIdIssuedAt(registeredClient.getClientIdIssuedAt());
+        entity.setClientSecret(registeredClient.getClientSecret());
+        entity.setClientSecretExpiresAt(registeredClient.getClientSecretExpiresAt());
+        entity.setClientName(registeredClient.getClientName());
+        entity.setClientAuthenticationMethods(StringUtils.collectionToCommaDelimitedString(clientAuthenticationMethods));
+        entity.setAuthorizationGrantTypes(StringUtils.collectionToCommaDelimitedString(authorizationGrantTypes));
+        entity.setRedirectUris(StringUtils.collectionToCommaDelimitedString(registeredClient.getRedirectUris()));
+        entity.setPostLogoutRedirectUris(StringUtils.collectionToCommaDelimitedString(registeredClient.getPostLogoutRedirectUris()));
+        entity.setScopes(StringUtils.collectionToCommaDelimitedString(registeredClient.getScopes()));
+        entity.setClientSettings(writeMap(registeredClient.getClientSettings().getSettings()));
+        entity.setTokenSettings(writeMap(registeredClient.getTokenSettings().getSettings()));
+
+        return entity;
+    }
+
+    private Map<String, Object> parseMap(String data) {
+        try {
+            return this.objectMapper.readValue(data, new TypeReference<Map<String, Object>>() {
+            });
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex.getMessage(), ex);
+        }
+    }
+
+    private String writeMap(Map<String, Object> data) {
+        try {
+            return this.objectMapper.writeValueAsString(data);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex.getMessage(), ex);
+        }
+    }
+
+    private static AuthorizationGrantType resolveAuthorizationGrantType(String authorizationGrantType) {
+        if (AuthorizationGrantType.AUTHORIZATION_CODE.getValue().equals(authorizationGrantType)) {
+            return AuthorizationGrantType.AUTHORIZATION_CODE;
+        } else if (AuthorizationGrantType.CLIENT_CREDENTIALS.getValue().equals(authorizationGrantType)) {
+            return AuthorizationGrantType.CLIENT_CREDENTIALS;
+        } else if (AuthorizationGrantType.REFRESH_TOKEN.getValue().equals(authorizationGrantType)) {
+            return AuthorizationGrantType.REFRESH_TOKEN;
+        }
+        return new AuthorizationGrantType(authorizationGrantType);
+    }
+
+    private static ClientAuthenticationMethod resolveClientAuthenticationMethod(String clientAuthenticationMethod) {
+        if (ClientAuthenticationMethod.CLIENT_SECRET_BASIC.getValue().equals(clientAuthenticationMethod)) {
+            return ClientAuthenticationMethod.CLIENT_SECRET_BASIC;
+        } else if (ClientAuthenticationMethod.CLIENT_SECRET_POST.getValue().equals(clientAuthenticationMethod)) {
+            return ClientAuthenticationMethod.CLIENT_SECRET_POST;
+        } else if (ClientAuthenticationMethod.NONE.getValue().equals(clientAuthenticationMethod)) {
+            return ClientAuthenticationMethod.NONE;
+        }
+        return new ClientAuthenticationMethod(clientAuthenticationMethod);
+    }
+}
