e4927e41523cfc3069856a8bf1925bae45bd8ccb
kimjihun <xxxjjhhh@naver.com>
2024-10-03 07:25:47 +0900

    * 인증 과정 정보를 담을 AuthorizationEntity 및 서비스 구현 close #7

---
+++ src/main/java/com/example/spring_oauth2_authorization_server/entity/AuthorizationEntity.java
@@ -0,0 +1,76 @@
+package com.example.spring_oauth2_authorization_server.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "oauth2_authorization")
+@Getter
+@Setter
+public class AuthorizationEntity {
+
+    @Id
+    @Column
+    private String id;
+    private String registeredClientId;
+    private String principalName;
+    private String authorizationGrantType;
+    @Column(columnDefinition = "TEXT")
+    private String authorizedScopes;
+    @Column(columnDefinition = "TEXT")
+    private String attributes;
+    @Column(length = 500)
+    private String state;
+
+    @Column(columnDefinition = "TEXT")
+    private String authorizationCodeValue;
+    private Instant authorizationCodeIssuedAt;
+    private Instant authorizationCodeExpiresAt;
+    private String authorizationCodeMetadata;
+
+    @Column(columnDefinition = "TEXT")
+    private String accessTokenValue;
+    private Instant accessTokenIssuedAt;
+    private Instant accessTokenExpiresAt;
+    @Column(length = 2000)
+    private String accessTokenMetadata;
+    private String accessTokenType;
+    @Column(length = 1000)
+    private String accessTokenScopes;
+
+    @Column(columnDefinition = "TEXT")
+    private String refreshTokenValue;
+    private Instant refreshTokenIssuedAt;
+    private Instant refreshTokenExpiresAt;
+    @Column(length = 2000)
+    private String refreshTokenMetadata;
+
+    @Column(columnDefinition = "TEXT")
+    private String oidcIdTokenValue;
+    private Instant oidcIdTokenIssuedAt;
+    private Instant oidcIdTokenExpiresAt;
+    @Column(length = 2000)
+    private String oidcIdTokenMetadata;
+    @Column(length = 2000)
+    private String oidcIdTokenClaims;
+
+    @Column(columnDefinition = "TEXT")
+    private String userCodeValue;
+    private Instant userCodeIssuedAt;
+    private Instant userCodeExpiresAt;
+    @Column(length = 2000)
+    private String userCodeMetadata;
+
+    @Column(columnDefinition = "TEXT")
+    private String deviceCodeValue;
+    private Instant deviceCodeIssuedAt;
+    private Instant deviceCodeExpiresAt;
+    @Column(length = 2000)
+    private String deviceCodeMetadata;
+}
